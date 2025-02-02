a5f4515dd3db52297592d3daf621c7bafb3bed1d
Lorien Aquarius <lorienaquarius@cmail.carleton.ca>
2025-02-02 01:59:07 -0500

    Initial commit, with trial code for playing around with motor encoder readings

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <pigpio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+
+#define MOTOR_ONE_DIRECTION_PIN 8
+#define MOTOR_ONE_ENABLE_PIN 7
+#define MOTOR_ONE_PULSE_PIN 12
+#define MOTOR_ONE_ENCODER_A_PIN 26
+#define MOTOR_ONE_ENCODER_B_PIN 19
+#define MOTOR_ONE_ENCODER_Z_PIN 13
+
+#define STEPS_PER_REVOLUTION 3844.585987
+#define MICROSTEPS = 2
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        cout << "Incorrect usage. Please enter degrees to turn to" << endl;
+        exit(1);
+    }
+    string arg = argv[1];
+    const int ENCODER_INCREMENTS[16] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+    int currentAB = 0;
+    int previousAB = 0;
+    int phaseA;
+    int phaseB;
+    int phaseZ;
+
+    if(gpioInitialise() < 0) {
+        cout << "Failed to initialize GPIO" << endl;
+        exit(1);
+    }
+
+    int result = 0;
+
+    result |= gpioSetMode(MOTOR_ONE_DIRECTION_PIN, PI_OUTPUT);
+    result |= gpioSetMode(MOTOR_ONE_ENABLE_PIN, PI_OUTPUT);
+    result |= gpioSetMode(MOTOR_ONE_PULSE_PIN, PI_OUTPUT);
+
+    result |= gpioSetMode(MOTOR_ONE_ENCODER_A_PIN, PI_INPUT);
+    result |= gpioSetMode(MOTOR_ONE_ENCODER_B_PIN, PI_INPUT);
+    result |= gpioSetMode(MOTOR_ONE_ENCODER_Z_PIN, PI_INPUT);
+
+    if (result > 0) {
+        cout << "Failed to set GPIO Modes" << endl;
+        exit(1);
+    }
+
+    int encoderCount = 0;
+
+    int targetEncoderCount = stoi(arg) * STEPS_PER_REVOLUTION / 360;
+
+    phaseA = gpioRead(MOTOR_ONE_ENCODER_A_PIN);
+    phaseB = gpioRead(MOTOR_ONE_ENCODER_B_PIN);
+    phaseZ = gpioRead(MOTOR_ONE_ENCODER_Z_PIN);
+    previousAB = (phaseA << 1) | phaseB;
+
+    gpioWrite(MOTOR_ONE_ENABLE_PIN, 1);
+    gpioWrite(MOTOR_ONE_DIRECTION_PIN, 0);
+
+    while (encoderCount < targetEncoderCount) {
+
+        gpioTrigger(MOTOR_ONE_PULSE_PIN, 1, 1);
+        sleep(0.000005);
+
+        phaseA = gpioRead(MOTOR_ONE_ENCODER_A_PIN);
+        phaseB = gpioRead(MOTOR_ONE_ENCODER_B_PIN);
+        phaseZ = gpioRead(MOTOR_ONE_ENCODER_Z_PIN);
+        currentAB = (phaseA << 1) + phaseB;
+        encoderCount += ENCODER_INCREMENTS[(previousAB << 2) | currentAB];
+
+    }
+}
