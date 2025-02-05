5f56899727f1335c980192a20393734d035c2a8e
Lorien Aquarius <lorienaquarius@cmail.carleton.ca>
2025-02-05 13:39:46 -0500

    Let's try finding the index again

--- main.cpp
+++ main.cpp
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
     gpioWrite(MOTOR_ONE_PULSE_PIN, 1);
     while (!gpioRead(MOTOR_ONE_ENCODER_Z_PIN)) {
         gpioTrigger(MOTOR_ONE_PULSE_PIN, 8, 0);
-        usleep(500);
+        usleep(5000);
 
         stepCount++;
         cout << "Current step count: " << stepCount << endl;
