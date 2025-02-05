26fa104a97401928589491d913f9983b5d6e44e6
Lorien Aquarius <lorienaquarius@cmail.carleton.ca>
2025-02-05 12:58:18 -0500

    Adjusted target encoder calculation

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]) {
 
     int stepCount = 0;
 
-    int targetStepCount = stoi(arg) * STEPS_PER_REVOLUTION / 360 * 4;
+    int targetStepCount = stoi(arg) * (360/400 * 19);
     cout <<"Target encoder count: " << targetStepCount << endl;
 
     // phaseA = gpioRead(MOTOR_ONE_ENCODER_A_PIN);
