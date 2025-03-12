252989429e1c382e76c42f8abf7b8343a699a268
sage7 <lorienaquarius@cmail.carleton.ca>
2025-03-12 19:00:42 +0000

    Testing out signal strength and speed on soldered breadboard

--- motor.h
+++ motor.h
@@ -12,7 +12,7 @@
 #define WORKING_PULSE_WIDTH 8
 #define MAX_STEP_SPEED 8
 #define WORKING_STEP_SPEED 8 // delay between steps in microseconds, while normal spinning
-#define WORKING_ENCODER_STEP_SPEED 1000 // Delay between steps in microseconds, while tryiing to use an encoder
+#define WORKING_ENCODER_STEP_SPEED 5000 // Delay between steps in microseconds, while tryiing to use an encoder
 
 class motor {
 
