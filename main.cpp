ab820d12728eb3c1167df3f535bc994f650b8756
sage7 <lorienaquarius@cmail.carleton.ca>
2025-03-18 20:59:33 +0000

    Changing the radar incoming format to just be numbers delimited by |

--- main.cpp
+++ main.cpp
@@ -16,35 +16,35 @@ using namespace std;
 ///
 /// @return 1 on failure, or don't return if everything is going well
 int main(){
-    string pipe_name = "radarpipe";
-    mkfifo(pipe_name.c_str(), 0666);
-    cout << "waiting for messages...";
-    int fd = open(pipe_name.c_str(), O_RDONLY);
-    if(fd == -1){
-        std::cerr << "Pipe error!";
-        return 1;
-    }
+    // string pipe_name = "radarpipe";
+    // mkfifo(pipe_name.c_str(), 0666);
+    // cout << "waiting for messages...";
+    // int fd = open(pipe_name.c_str(), O_RDONLY);
+    // if(fd == -1){
+    //     std::cerr << "Pipe error!";
+    //     return 1;
+    // }
 
-    char buffer[128];
+    char buffer_raw[128];
+    string buffer(buffer_raw);
     radarData data;
-    while (true){
-        ssize_t bytesRead = read(fd,buffer,sizeof(buffer)-1);
-        if (bytesRead >0){
-            buffer[bytesRead] = '\0';
-            data.target = regex_match(buffer, regex("(?:Target )[0-9]"));
-            data.posX = regex_search(buffer, regex("(?:pos=\()[0-9]*\.[0-9]*"));
-            data.posY = regex_search(buffer, regex("(?:pos=\([0-9]*\.[0-9]*,) [0-9]*\.[0-9]*"));
-            data.posZ = regex_search(buffer, regex("(?:pos=\(([0-9]*\.[0-9]*, ){2})[0-9]*\.[0-9]*"));
-            data.velX = regex_search(buffer, regex("(?:vel=\()[0-9]*\.[0-9]*"));
-            data.velY = regex_search(buffer, regex("(?:vel=\([0-9]*\.[0-9]*,) [0-9]*\.[0-9]*"));
-            data.velZ = regex_search(buffer, regex("(?:vel=\(([0-9]*\.[0-9]*, ){2})[0-9]*\.[0-9]*"));
-            data.accX = regex_search(buffer, regex("(?:acc=\()[0-9]*\.[0-9]*"));
-            data.accY = regex_search(buffer, regex("(?:acc=\(-?[0-9]*\.[0-9]*, )-?[0-9]*\.[0-9]*"));
-            data.accZ = regex_search(buffer, regex("(?:acc=\((-?[0-9]*\.[0-9]*, ){2})-?[0-9]*\.[0-9]*"));
+    cmatch match;
+    cout << "Please put in a data string" << endl;
+    cin >> buffer;
 
+    data.target = stoi(buffer.substr(0, buffer.find(DATA_DELIMITER)));
+    data.posX = stod(buffer.substr(1, buffer.find(DATA_DELIMITER)));
+    data.posY = stod(buffer.substr(2, buffer.find(DATA_DELIMITER)));
+    data.velX = stod(buffer.substr(3, buffer.find(DATA_DELIMITER)));
+    data.velY = stod(buffer.substr(4, buffer.find(DATA_DELIMITER)));
+    data.velZ = stod(buffer.substr(5, buffer.find(DATA_DELIMITER)));
+    data.accX = stod(buffer.substr(6, buffer.find(DATA_DELIMITER)));
+    data.accY = stod(buffer.substr(7, buffer.find(DATA_DELIMITER)));
+    data.accZ = stod(buffer.substr(8, buffer.find(DATA_DELIMITER)));
 
-        }
-    }
-    close(fd);
-    return 0;
+    cout << "Parsed radar data" << endl;
+    cout << "Target number: " << data.target << endl;
+    cout << "Position Data: X: " << data.posX << " Y: " << data.posY << " Z: " << data.posZ << endl;
+    cout << "Velocity Data: X: " << data.velX << " Y: " << data.velY << " Z: " << data.velZ << endl;
+    cout << "Acceleration Data: X: " << data.accX << " Y: " << data.accY << " Z: " << data.accZ << endl;
 }
